7943e68650f0e4b956dd99f5772f4b09f33ccc7c
Eddy <caleroeddy99@gmail.com>
2025-10-04 20:38:56 -0600

    Terminando la guia 2 y probando en Swagger

--- my_first_proyect_ejcp/src/users/dto/create-user.dto.ts
+++ my_first_proyect_ejcp/src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+
+import { ApiProperty } from "@nestjs/swagger";
+import { create } from "domain";
+
+export class CreateUserDto {
+        @ApiProperty({required: true, example: 'usuario@empresa.com'})
+        
+        email:string;
+
+        @ApiProperty({required: true, example: 'Eddy Calero'})
+
+        name: string;
+
+        username?: string;
+
+        @ApiProperty({required: true, example:'password123'})
+
+        password: string;
+
+        @ApiProperty({readOnly: true, example: 1, description: 'id del tenant'})
+        tenantId: number;
+
+}
+
